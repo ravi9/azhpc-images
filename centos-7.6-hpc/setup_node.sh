3ae5c8ee7ab1aba2f35edb65e0cc3b4f3f7dcc65
Jithin Jose <jijos@microsoft.com>
2019-02-14 08:27:58 -0800

    Add modulefile for gcc-8.2.0

--- centos-7.6-hpc/setup_node.sh
+++ centos-7.6-hpc/setup_node.sh
@@ -12,11 +12,13 @@ set -ex
 
 # Install gcc 8.2
 ./install_gcc-8.2.sh
+cp ./modulefiles/gcc-8.2.0 /usr/share/Modules/modulefiles/
+module load gcc-8.2.0
 
 # Install MPI libraries and Communication Runtimes
 ./install_mpi.sh
 
 # Setup environment modules for MPI libraries
 mkdir -p /usr/share/Modules/modulefiles/mpi/
-cp ./modulefiles/* /usr/share/Modules/modulefiles/mpi/
+cp ./modulefiles/mpi/* /usr/share/Modules/modulefiles/mpi/
 
