e9eef51a0c8bc785991c8d3f930ec785b544597f
root <root@node2.e0bdba3bf5rerigbgjmzubahxe.jx.internal.cloudapp.net>
2019-02-21 16:00:35 +0000

    Remove extra packages

--- centos-7.6-hpc/install_dev_tools.sh
+++ centos-7.6-hpc/install_dev_tools.sh
@@ -4,11 +4,9 @@ set -ex
 # Install pre-reqs and development tools
 yum groupinstall -y "Development Tools"
 yum install -y numactl numactl-devel libxml2-devel byacc environment-modules
-yum install -y python-devel python-setuptools redhat-rpm-config rpm-build
-yum install -y gtk2 atk cairo tcl tk createrepo emacs
-
-yum install -y redhat-rpm-config rpm-build gcc-gfortran gcc-c++
-yum install -y gtk2 atk cairo tcl tk createrepo wget
+yum install -y python-devel python-setuptools
+yum install -y gtk2 atk cairo tcl tk
+yum install -y gcc-gfortran gcc-c++
 KERNEL=$(uname -r)
 yum install -y kernel-devel-${KERNEL}
 yum install -y m4 libgcc.i686 glibc-devel.i686
